d6fb9d0972fc3f11674f3966c4978eca2c3ff149
humblehound <dejtabejz@gmail.com>
2016-03-30 12:02:11 +0200

    Plotter separation

--- parser.py
+++ parser.py
@@ -1,20 +1,14 @@
-from pylab import*
 from scipy.io import wavfile
-import matplotlib.pyplot as plt
 
 frequency, sound = wavfile.read('wavfiles/testFile.wav')
 
+#each sound consists of 2 channels
+
+#in order to compute some of the properties, we need to perform
+#sound normalization - convert each frame into the range of [-1,1]
+
 # get the first channel
 channel1 = sound[:,0] 
-# x axis - all the frames
-#timeArray = arange(0, channel1.shape[0], 1)
-#timeArray = timeArray / frequency
-#print timeArray
-#timeArray = timeArray * 1000  #scale to milliseconds
 
-#plt.plot(timeArray, channel1, color='k')
-#ylabel('Amplitude')
-#xlabel('Time (ms)')
 
-#plt.show()
 
---
+++ plotter.py
@@ -0,0 +1,11 @@
+# x axis - all the frames
+#timeArray = arange(0, channel1.shape[0], 1)
+#timeArray = timeArray / frequency
+#print timeArray
+#timeArray = timeArray * 1000  #scale to milliseconds
+
+#plt.plot(timeArray, channel1, color='k')
+#ylabel('Amplitude')
+#xlabel('Time (ms)')
+
+#plt.show()

d81d4e7dfe88450e55ce14643d025a3c387f1455
rolnikrolnik <lukaszdawidowski@outlook.com>
2016-04-03 20:21:13 +0200

    modified dockerfile to include mfcc library, added mfcc invocation and wav duration to parser

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,17 @@ MAINTAINER Humblehound <dejtabejz@gmail.com>
 RUN apt-get update && apt-get install -y \
     python-numpy \
     python-scipy \
-    python-pip
+    python-pip \
+    wget \
+    unzip
 
 RUN pip install jsonpickle
+
+# install dependencies-
+RUN mkdir downloads
+RUN wget --no-check-certificate https://github.com/jameslyons/python_speech_features/archive/master.zip
+RUN unzip master.zip -d /downloads/
+RUN cd /downloads/python_speech_features-master/ && python setup.py install
 
 CMD "mkdir app"
 COPY parser.py app/
--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 from scipy.io import wavfile
+from features import mfcc
 from jsonpickle import encode
 
 wavFilesPath = 'wavfiles/'
-
 
 class Sound(object):
     def __init__(self, filename):
@@ -10,13 +10,16 @@ class Sound(object):
         self.frequency, self.sound = wavfile.read(wavFilesPath + filename)
         self.channel1 = self.sound[:, 0]
         self.channel2 = self.sound[:, 1]
+        self.duration = len(self.sound) / self.frequency
+        self.mfccFeatures = mfcc(self.sound, self.frequency)
 
 
 class ResultFile(object):
     def __init__(self, soundfile):
         self.filename = soundfile.filename
         self.frequency = soundfile.frequency
-
+        self.duration = soundfile.duration
+        self.mfccLength = len(soundfile.mfccFeatures)
 
 soundFile = Sound('classic.wav')
 resultFile = ResultFile(soundFile)

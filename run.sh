69f0087b1c1bc8c780df2a0eb6ca20381b053067
Humblehound <dejtabejz@gmail.com>
2016-04-21 15:49:17 +0200

    new configuration

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 docker build -t humblehound/pythonparser:latest .
-docker rm parser
-docker run --name=parser humblehound/pythonparser:latest
+docker run --name=parser --rm -v $(pwd)/data:/app/data -v $(pwd)/results:/app/results humblehound/pythonparser:latest

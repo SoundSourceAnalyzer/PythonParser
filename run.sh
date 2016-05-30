d27ae005a636aae47cb2953ed072c516711db919
rolnikrolnik <lukaszdawidowski@outlook.com>
2016-05-31 00:27:16 +0200

    WIP

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 #set -e
-docker build -t humblehound/pythonparser:latest .
-docker run --rm --name=parser -v /opt/genres/:/app/genres humblehound/pythonparser:latest
+# docker build -t humblehound/pythonparser:latest .
+# docker run --rm --name=parser -v /opt/genres/:/app/genres humblehound/pythonparser:latest
+
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+set -eu
+
+docker run -p 8888:8888 -v $(pwd):/notebooks -it --rm pcej/keras-jupyter-yaafe    
